b6a52c708d1c10bb9f803f3f9f69f02772d3969d
ruhidniftiyev <tigerrn22@gmail.com>
2023-11-18 22:38:18 +0400

    change header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,11 +16,14 @@ const Header = (props: Props) => {
       <div className="w-11/12 flex justify-between">
         <div className="flex">
           <img className="w-16 mt-2" src={logoImg} alt="" />
-          <h1 className="header__logo-text font-semibold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-regal-blue to-regal-pink mt-5 ml-3">
-            evim.az
+          <h1 className="header__logo-text font-semibold text-transparent text-3xl bg-clip-text text-white mt-5 ml-3">
+            evim
+          </h1>
+          <h1 className="header__logo-text font-semibold text-transparent text-3xl bg-clip-text text-regal-orange mt-5 ml-0">
+            .az
           </h1>
         </div>
-        <div className="flex items-center w-1/3 justify-around text-lg">
+        <div className="flex items-center w-1/3 justify-around text-lg text-white">
           <div className="">
             <div className="flex">
               <img className="w-7 h-7" src={langImg} alt="" />
@@ -34,7 +37,7 @@ const Header = (props: Props) => {
             <p className="font-medium ml-2">Seçilmişlər</p>
           </div>
           <div className="flex cursor-pointer">
-            <p className="border-2 border-white rounded-full text-xl w-7 h-7 flex items-center text-[#ffffff80] justify-center font-medium hover:bg-[#FFA500] hover:text-white ">
+            <p className="border-2 border-white rounded-full text-xl w-7 h-7 flex items-center  justify-center font-medium hover:bg-[#FFA500] hover:text-white ">
               +
             </p>
             <p className="ml-2 font-medium">Elan yerləşdir</p>
