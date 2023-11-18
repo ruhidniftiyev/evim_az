bba4c5d4b1f57b6091c549892ca304a645698c95
ruhidniftiyev <tigerrn22@gmail.com>
2023-11-18 22:51:40 +0400

    change loader color

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import LoadingPage from './pages/LoadingPage';
 import AppRouter from './components/AppRouter';
 
 function App() {
-  // const isLoading = useAppSelector((store) => store.houseSlice.isLoading);
+  const isLoading = useAppSelector((store) => store.houseSlice.isLoading);
 
   return (
     // <div className="w-11/12 m-auto p-3">
     <div>
-      {/* {isLoading && <LoadingPage />} */}
+      {isLoading && <LoadingPage />}
       <AppRouter />
     </div>
   );
